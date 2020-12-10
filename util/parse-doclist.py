56664d1e75ec1661c784c0bca46a187808d705ea
DongDu <ddnirvana1@gmail.com>
2020-12-10 19:19:23 +0800

    init
    Signed-off-by: DongDu <ddnirvana1@gmail.com>

---
+++ util/parse-doclist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import json
+
+codeDocList = {}
+
+def load_doclist(doclist_name):
+    with open(doclist_name, 'r') as f:
+        data = json.load(f)
+        return data
+
+def doc_of_the_code(code_name):
+    value = codeDocList.get(code_name)
+    if value:
+        return value
+    else:
+        return 'OMGNoDocFound'
+
+# The following code is for test
+codeDocList = load_doclist("doclist.json")
+print(codeDocList)
+
+print(doc_of_the_code('hello.c'))
+print(doc_of_the_code('world.c'))
+print(doc_of_the_code('test.c'))
+print(doc_of_the_code('bye.c'))
